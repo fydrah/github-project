2283a956a39b25fa12f3bf4fa3eb42420c624166
Jacques Roussel <jacques.roussel@objectif-libre.com>
2018-05-22 10:06:12 +0200

    Modify Jenkinsfile to configure the slave runner dynamically

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,23 +3,35 @@ import org.automation.*
 
 def python = new Python()
 
-node ("python27") {
-  stage("Setup"){
-    deleteDir()
-    checkout scm
-    python.pipInstallRequirements("./requirements.txt")
-  }
-  stage("Create GitHub Project"){
-    python.execScript("create_project.py", [
-      "--name", "${PROJECT_NAME}",
-      "--type", "user"
-    ])
-  }
-  stage("Create Jenkins Job"){
-    python.execScript("create_base_job.py", [
-      "--name", "${PROJECT_NAME}",
-      "--git", "${PROJECT_GIT}",
-      "--insecure"
-    ])
-  }
+podTemplate(label: 'python27', cloud: 'openshift', containers: [
+  containerTemplate(name: 'python27', image: 'jarou/jenkins-slave-python27-centos7', ttyEnabled: true, command: 'cat', envVars: [envVar(key: 'BASH_ENV', value: '/home/jenkins/venv/bin/activate'), envVar(key: 'ENV', value: '/home/jenkins/venv/bin/activate')])
+  ]) {
+
+    node ("python27") {
+      stage("Setup"){
+        deleteDir()
+        checkout scm
+        container('python27') {
+          python.pipInstallRequirements("./requirements.txt")
+        }
+      }
+      stage("Create GitHub Project"){
+        container('python27') {
+          python.execScript("create_project.py", [
+            "--name", "${PROJECT_NAME}",
+            "--type", "user"
+          ])
+        }
+      }
+      stage("Create Jenkins Job"){
+        container('python27') {
+          python.execScript("create_base_job.py", [
+            "--name", "${PROJECT_NAME}",
+            "--git", "${PROJECT_GIT}",
+            "--insecure"
+          ])
+        }
+      }
+    }
+
 }
